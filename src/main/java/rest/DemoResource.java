14682b65a5967461347cef3a72d6341c8c6f5450
Patrick Jahn <Patrick.jackobsen.jahn.109@gmail.com>
2021-01-12 14:45:35 +0100

    Test removed

--- src/main/java/rest/DemoResource.java
+++ src/main/java/rest/DemoResource.java
@@ -104,98 +104,11 @@ public class DemoResource {
     
     
     
-    /** OBS Nedestående endpoints er til for at vise hvordan man kan hente fra andre servere OBS  **/
-    
-    @GET
-    @RolesAllowed({"admin","user"})
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("filmsparallel")
-    public Response getFromServersParallel() throws IOException, InterruptedException, ExecutionException, API_Exception {
-            return Response.ok()
-               .header("Access-Control-Allow-Origin", "*")
-               .header("Access-Control-Allow-Credentials", "true")
-               .header("Access-Control-Allow-Headers","origin, content-type, accept, authorization")
-               .header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS, HEAD")
-               .entity( remoteFACADE.getAllFilmsParallel()).build();
-    }
-    
-     @GET
-     @RolesAllowed({"admin","user"})
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("films")
-    public Response getFromServers() throws IOException, API_Exception {
-        return Response.ok()
-               .header("Access-Control-Allow-Origin", "*")
-               .header("Access-Control-Allow-Credentials", "true")
-               .header("Access-Control-Allow-Headers","origin, content-type, accept, authorization")
-               .header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS, HEAD")
-               .entity( remoteFACADE.getAllFilms()).build();
-
-    }
+  
     
     
     
-     /** OBS Nedestående endpoints er tilføjet som ekstra i personlig CA3 OBS  **/
     
-    @POST
-    @RolesAllowed({"admin","user"})
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes({MediaType.APPLICATION_JSON})
-    @Path("likefilm")
-    public Response likeMovie(String jsonString) {
-        
-        JsonObject json = JsonParser.parseString(jsonString).getAsJsonObject();
-           String username = json.get("username").getAsString();
-           String url = json.get("url").getAsString();
-      
-            FACADE.addLikedMovie(username, url);
-           return Response.ok()
-               .header("Access-Control-Allow-Origin", "*")
-               .header("Access-Control-Allow-Credentials", "true")
-               .header("Access-Control-Allow-Headers","origin, content-type, accept, authorization")
-               .header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS, HEAD")
-               .entity(  "{\"msg\":\"Movie with url " + url + " was liked by " + username + " \"}").build();
-
-    }
-    
-    
-     @POST
-    @RolesAllowed({"admin","user"})
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes({MediaType.APPLICATION_JSON})
-    @Path("dislikefilm")
-    public Response dislikeMovie(String jsonString) {
-        
-        JsonObject json = JsonParser.parseString(jsonString).getAsJsonObject();
-           String username = json.get("username").getAsString();
-           String url = json.get("url").getAsString();
-      
-            FACADE.removeLikedMovie(username, url);
-             return Response.ok()
-               .header("Access-Control-Allow-Origin", "*")
-               .header("Access-Control-Allow-Credentials", "true")
-               .header("Access-Control-Allow-Headers","origin, content-type, accept, authorization")
-               .header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS, HEAD")
-               .entity("{\"msg\":\"Movie with url " + url + " was disliked by " + username + " \"}").build();
-
-    }
-    
-    @GET
-    @RolesAllowed({"admin","user"})
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("allfilms")
-    public Response getFromServersAll() throws IOException, API_Exception, InterruptedException, ExecutionException {
-         EntityManager em = EMF.createEntityManager();
-                 String thisuser = securityContext.getUserPrincipal().getName();
-       User u = em.find(User.class,thisuser);   
-       return Response.ok()
-               .header("Access-Control-Allow-Origin", "*")
-               .header("Access-Control-Allow-Credentials", "true")
-               .header("Access-Control-Allow-Headers","origin, content-type, accept, authorization")
-               .header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS, HEAD")
-               .entity(remoteFACADE.getAllFilmsParallel2(u.getLikedMovies())).build();
-
-        
-    }
+   
     
 }   
